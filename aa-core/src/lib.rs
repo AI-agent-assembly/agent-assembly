0272e3919ffb59c0d070c2b264116d91515f1980
Chisanan232 <chi10211201@cycu.org.tw>
2026-04-27 09:44:30 +0800

    ✨ (aa-core): Add cfg_if block for conditional alloc extern

--- aa-core/src/lib.rs
+++ aa-core/src/lib.rs
@@ -5,3 +5,9 @@
 //! It has no runtime or I/O dependencies.
 
 #![cfg_attr(not(feature = "std"), no_std)]
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "alloc")] {
+        extern crate alloc;
+    }
+}
