acc40f2be14700dfdecda9390508ddc76e7e590a
Chisanan232 <chi10211201@cycu.org.tw>
2026-04-27 12:08:33 +0800

    🧱 (ffi-go): Add C header scaffold for Go bindings

---
+++ aa-ffi-go/include/aa_ffi_go.h
@@ -0,0 +1,14 @@
+#ifndef AA_FFI_GO_H
+#define AA_FFI_GO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct aa_client_handle aa_client_handle;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // AA_FFI_GO_H
