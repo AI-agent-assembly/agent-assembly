3273377fd81921ca5e916e0e72a29677c68660ba
Chisanan232 <chi10211201@cycu.org.tw>
2026-05-02 22:09:03 +0800

    🐛 fix(proto): move rerun-if-changed before proto_root is moved into slice

--- aa-proto/build.rs
+++ aa-proto/build.rs
@@ -18,13 +18,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         proto_root.join("event.proto"),
     ];
 
+    // Re-run this build script if any proto file changes.
+    println!("cargo:rerun-if-changed={}", proto_root.display());
+
     tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
         .compile_protos(&proto_files, &[proto_root])?;
-
-    // Re-run this build script if any proto file changes.
-    println!("cargo:rerun-if-changed={}", proto_root.display());
 
     Ok(())
 }
