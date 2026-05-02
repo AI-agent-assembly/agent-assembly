d9a6db42c692095f56c78399a7a80eb187f7aed4
Chisanan232 <chi10211201@cycu.org.tw>
2026-05-02 22:06:00 +0800

    🐛 fix(proto): fix compile_protos include path borrow in build.rs
    tonic-prost-build 0.14 takes &[PathBuf] for include dirs, not &[&PathBuf].

--- aa-proto/build.rs
+++ aa-proto/build.rs
@@ -21,7 +21,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
-        .compile_protos(&proto_files, &[&proto_root])?;
+        .compile_protos(&proto_files, &[proto_root])?;
 
     // Re-run this build script if any proto file changes.
     println!("cargo:rerun-if-changed={}", proto_root.display());
